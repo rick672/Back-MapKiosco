a8f868ffc58a1e1aa3e66f71654e65fc1b0b3677
rick672 <aliagaricardo321@gmail.com>
2026-04-01 22:57:28 -0400

    migrations y controller login

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+    public function login(Request $request)
+    {
+        $credenciales = $request->only('email', 'password');
+        return $credenciales;
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+});
